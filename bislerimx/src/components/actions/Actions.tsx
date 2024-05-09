a673564f91f5487d559fe317f6afbc3fea05a2a9
ShresthaAnush <anushshrestha421@gmail.com>
2024-05-09 17:33:01 +0545

    Added multiple components like Navbar, Searchbar and Logo for the project

---
+++ bislerimx/src/components/actions/Actions.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Actions.css"
+
+export default  function Actions() {
+    return (
+        <div>Actions</div>
+    )
+}
---
+++ bislerimx/src/components/searchbar/Searchbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./Searchbar.css"
+
+export default  function Searchbar() {
+    return <div>Searchbar</div>;
+    
+}
